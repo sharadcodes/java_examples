36f9ea0b3e5f247d14c091a8e0695e0986a3fd8a
SHARAD RAJ <36638057+sharadcodes@users.noreply.github.com>
2019-10-30 02:13:17 +0530

    Create mynote.sql

---
+++ sql_files/mynote.sql
@@ -0,0 +1,13 @@
+-- run these one by one      , Author: Sharad Raj, GIHSM, Lucknow https://linkedin.com/in/srsm
+
+CREATE DATABASE `mynote`;
+
+USE `mynote`;
+
+CREATE TABLE `notes` (
+  `note_id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` mediumtext NOT NULL,
+  `content` longtext,
+  PRIMARY KEY (`note_id`),
+  UNIQUE KEY `note_id_UNIQUE` (`note_id`)
+);
